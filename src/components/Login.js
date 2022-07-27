33e29376ce341e1b635bff43fb3ce856163b4f78
Judy Mwai <judymukami1996@gmail.com>
2022-07-27 20:29:31 +0300

    adds login form in the login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,25 @@ import React from "react"
 function Login (){
     return (
         <div>
+            <form>
+                <div>
+                    <input type="text" name="username" placeholder="Username" />
+                    
+                </div>
+                <div>
+                    <input type="password" name="password" placeholder="Password" />
+
+                </div>
+                <div>
+                    <input type="submit" value="Submit" />
+
+                </div>
+
+                
+                
+            </form>
+            
+            
 
         </div>
     )
