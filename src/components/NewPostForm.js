4805c5af5835140ff5c95821ac92e6ba8635a571
Judy Mwai <judymukami1996@gmail.com>
2022-07-27 21:03:07 +0300

    defines states for various imputs in the newpostform component

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,9 +1,14 @@
 import React from "react";
 
 export default function NewPostForm(){
+    const [title, setTitle]=useState('')
+    const [content, setContent]=useState('')
+    const [author, setAuthor]=useState('')
+
+
     return (
         <div>
-            
+
         </div>
     )
 }
