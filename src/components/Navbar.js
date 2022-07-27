41950609ba6676ba526a1c839095d7ab4d006411
Judy Mwai <judymukami1996@gmail.com>
2022-07-27 20:48:20 +0300

    adds navliink in the navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,11 @@ import React from "react"
 function Navbar(){
     return(
         <div>
+            <li><NavLink to="/" exact>HomePage</NavLink></li>
+            {/* <li><NavLink to="/posts" exact>Posts</NavLink></li> */}
+            <li><NavLink to="/login" exact>Login</NavLink></li>
             
+
         </div>
     )
 }
