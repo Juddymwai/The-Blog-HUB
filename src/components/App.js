32b0e0594136b85a08e4d7b280b3607fc2553dbb
Judy Mwai <judymukami1996@gmail.com>
2022-07-27 19:59:54 +0300

    creates components

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function App() {
+  return (
+    <center style={{color: "red", fontSize: 80, marginTop:30+"vh"}}>
+      <h1>Huuurrrrraaaaayyyyy</h1>
+    </center>
+  );
+}
+
+export default App;
