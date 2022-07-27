1f9189a92d51094bc40120eaabc42ecd6e8c9813
Judy Mwai <judymukami1996@gmail.com>
2022-07-27 20:43:38 +0300

    adds the navbar component

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect}from "react";
 import Mojo from "./Mojo";
-import NewPostForm from "./NewPostForm"
+// import NewPostForm from "./NewPostForm"
 
 
 function HomePage(){
