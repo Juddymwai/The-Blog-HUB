a5c49242ae6f33a581f14533cc6d409443b03c97
Judy Mwai <judymukami1996@gmail.com>
2022-07-27 21:11:51 +0300

    adds a handlesubmit function and fetch to post new data

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -5,6 +5,26 @@ export default function NewPostForm(){
     const [content, setContent]=useState('')
     const [author, setAuthor]=useState('')
 
+    function handleSubmit(e){
+        e.preventDefault()
+
+        const newObj ={
+            "Title":title,
+            "Content":content,
+            "Author": author
+        }
+        fetch('http://localhost:4000/posts',{
+            method:"POST",
+            headers:{'Context-Type':"application/json"},
+            body: JSON.stringify(newObj),
+
+
+        })
+        .then((response)=> response.json())
+        .then ((newData)=> onHandleAddPost(newData))
+
+    }
+
 
     return (
         <div>
