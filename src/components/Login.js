0f8ebb40560f21da36ad20fb5fa1b81e39bbb8d4
Judy Mwai <judymukami1996@gmail.com>
2022-07-28 14:15:43 +0300

    adds styling for the login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,29 +3,26 @@ import {Link} from "react-router-dom"
 
 function Login (){
     return (
-        <div>
-            <h2>Login Form</h2>
+        <div style={{margin: 50+ "px"}}>
             <form>
-                <div>
-                    <input type="text" name="username" placeholder="Username" required/>
-                    
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required/>
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
-                <div>
-                    <input type="password" name="password" placeholder="Password" required />
-
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" required/>
                 </div>
-                <div>
-                    <Link to="/publish"><input type="submit" value="Submit"/> </Link>
-
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
                 </div>
-                
+                <Link to="/publish"><input type="submit" value="Submit" required/> </Link>
             </form>
-            
-
-            
-            
-
         </div>
+        
+       
         
     )
 }
